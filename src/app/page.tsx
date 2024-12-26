f0879ada8fc29097a4676c173c7668c75f62b91f
masudkhan-dev <masudkhan.dev@gmail.com>
2024-12-26 22:50:23 +0600

    update scroll to in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Contact from "@/components/Home/Contact/Contact";
 import Hero from "@/components/Home/Hero/Hero";
 import Projects from "@/components/Home/Projects/Projects";
--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -1,13 +1,23 @@
-import Link from "next/link";
+"use client";
 
 const Footer = () => {
+  const handleScroll = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       <footer className="border-t-4 border-[#0a0a0a]">
         <div className="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
-          <Link href="/" className="font-bold text-xl text-[#A8D5BA]">
+          <button
+            onClick={handleScroll}
+            className="font-bold text-xl text-[#A8D5BA]"
+          >
             Masudur Rahman
-          </Link>
+          </button>
 
           <p className="text-sm text-[#f8f9fa]">
             &copy; Copyright 2024 - {new Date().getFullYear()} All Rights
